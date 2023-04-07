36ee952a404b392af1efeae73cbee205140b92db
Xiao <jl3798@gmail.com>
2023-04-06 20:10:12 -0400

    Final edit

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 function buildCharts(sample){
     d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) =>{
         let samples = data.samples;
-        let resultArray = samples.filter((sampleDictionary) => sampleDictionary.id ==sample);
+        let resultArray = samples.filter((sampleDictionary) => sampleDictionary.id == sample);
         let result = resultArray[0];
 
         let otuIDs = result.otu_ids;
@@ -30,7 +30,7 @@ function buildCharts(sample){
 
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-        let yticks = otuIDs.slice(0,10).map(otuID => 'OTU ${otuID}').reverse();
+        let yticks = otuIDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
         let barData = [
             {
                 y: yticks,
